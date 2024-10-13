b9a00c8c4a7279a3a6f07b956dad83b2e1caf103
dpills <dpills@protonmail.com>
2024-10-13 08:15:23 -0400

    React MUI Project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Zakamo
+
+React project using Material UI.
+
+## Development
+
+Install dependencies
+
+```bash
+$ bun install v1.1.26 (0a37423b)
+
+Checked 312 installs across 349 packages (no changes) [29.00ms]
+```
+
+Start the local development server
+
+```bash
+$ bun run start
+
+  VITE v5.4.8  ready in 174 ms
+
+  ➜  Local:   http://localhost:3000/
+  ➜  Network: http://192.168.50.102:3000/
+  ➜  press h + enter to show help
+```
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    include: ["@emotion/react", "@emotion/styled", "@mui/icons-material"],
+  },
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    open: true,
+  },
+})
